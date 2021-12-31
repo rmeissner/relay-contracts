e724f46c683fac6c55d2ea20923b1b8bb9cfe244
Richard Meissner <msc.meissner@gmail.com>
2021-12-31 16:37:32 +0100

    Initial commit

---
+++ contracts/RelayModuleFixedReward.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity >=0.7.0 <0.9.0;
+/// @title Relay Module with fixed Reward
+/// @author Richard Meissner - @rmeissner
+contract RelayModuleFixedReward {
+    string public constant VERSION = "1.0.0";
+}
